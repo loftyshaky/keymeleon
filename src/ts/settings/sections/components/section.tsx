22838e5593548fd59497cfcde3f7c72a891c676a
loftyshaky <loftyshaky@gmail.com>
2026-05-16 16:25:40 +1000

    feat(ui): change how input width is set

--- src/ts/settings/sections/components/section.tsx
+++ src/ts/settings/sections/components/section.tsx
@@ -6,16 +6,19 @@ import React, { useEffect } from 'react';
 import { observer } from 'mobx-react';
 
 import { d_offers } from '@loftyshaky/shared-app/shared';
-import { c_inputs, o_inputs, i_inputs } from '@loftyshaky/shared-app/inputs';
+import { c_inputs, d_inputs, o_inputs, i_inputs } from '@loftyshaky/shared-app/inputs';
 import { d_settings } from 'shared/internal';
 import { c_sections, d_sections, s_sections, i_sections } from 'settings/internal';
 
 export const Section: React.FunctionComponent = observer(() => {
+    const { locale } = data.settings.prefs;
     useEffect(() => {
         (async () => {
-            await d_sections.InputWidth.calculate();
+            await d_inputs.InputWidth.calculate({
+                set_all_inputs_to_msg_input_min_width_css: true,
+            });
         })();
-    });
+    }, [locale]);
 
     const generate_sections = () =>
         err(() => {
--- src/ts/settings/sections/data/index.ts
+++ src/ts/settings/sections/data/index.ts
@@ -1,4 +1,3 @@
-export * from 'settings/sections/data/input_width';
 export * from 'settings/sections/data/options';
 export * from 'settings/sections/data/sections';
 export * from 'settings/sections/data/btn_bar';
