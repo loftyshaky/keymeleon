98b073befd0904b4ae1e73f14d2eb54f75888f62
loftyshaky <loftyshaky@gmail.com>
2026-05-16 19:10:20 +1000

    feat(ui): add blaze theme

--- src/ts/settings/sections/data/options.ts
+++ src/ts/settings/sections/data/options.ts
@@ -48,6 +48,7 @@ class Class {
                     new o_inputs.Option({ name: 'clover' }),
                     new o_inputs.Option({ name: 'aqua' }),
                     new o_inputs.Option({ name: 'lavender' }),
+                    new o_inputs.Option({ name: 'blaze' }),
                     new o_inputs.Option({ name: 'ruby' }),
                 ],
             };
